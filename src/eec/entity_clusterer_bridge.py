17a2063b1b0a007607fe24619735755df198f5cf
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2023-02-09 01:45:43 +0300

    EnttitClustererBridge changed to singelton

--- src/eec/entity_clusterer_bridge.py
+++ src/eec/entity_clusterer_bridge.py
@@ -1,4 +1,6 @@
-from eec.implementations.base import *
+from eec.interfaces.interface_cluster_repository.i_cluster_repository import IClusterRepository
+from eec.interfaces.interface_mention_clustering_method.i_mention_clustering_method import IMentionClusteringMethod
+from eec.interfaces.interface_entity_repository.i_entity_repository import IEntityRepository
 
 
 class EntityClustererBridge:
@@ -7,16 +9,23 @@ class EntityClustererBridge:
         It does not contain any logic.
     '''
 
-    def __init__(self, entity_repository: BaseEntityRepository,
-                 cluster_repository: BaseClusterRepository,
-                 mention_clustering_method: BaseMentionClusteringMethod):
-        self.entity_repository = entity_repository
-        self.cluster_repository = cluster_repository
-        self.mention_clustering_method = mention_clustering_method
+    def __init__(self):
+        self.entity_repository = None
+        self.cluster_repository = None
+        self.mention_clustering_method = None
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, 'instance'):
             cls.instance = super(EntityClustererBridge, cls).__new__(cls)
         return cls.instance
+
+    def set_entity_repository(self, entity_repository: IEntityRepository):
+        self.entity_repository = entity_repository
+
+    def set_cluster_repository(self, cluster_repository: IClusterRepository):
+        self.cluster_repository = cluster_repository
+
+    def set_mention_clustering_method(self, mention_clustering_method: IMentionClusteringMethod):
+        self.mention_clustering_method = mention_clustering_method
 
     
