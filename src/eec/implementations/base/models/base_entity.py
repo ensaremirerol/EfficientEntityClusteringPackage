36287d1f35d948357c51aa02a3392d715a771985
Ensar Emir EROL <ensaremir.erol99@gmail.com>
2023-02-09 16:03:02 +0300

    base entity from_dict fix

--- src/eec/implementations/base/models/base_entity.py
+++ src/eec/implementations/base/models/base_entity.py
@@ -8,7 +8,7 @@ class BaseEntity(IEntity):
     def __init__(
             self, mention: str, entity_id: str, entity_source: str, entity_source_id: str,
         cluster_id: Optional[str] = None,
-            mention_vector: np.ndarray = np.array([])):
+            mention_vector: np.ndarray = np.array([]), has_mention_vector: bool = False):
         super().__init__(mention, entity_id, entity_source, entity_source_id, cluster_id is not None, cluster_id)
         self.mention_vector: np.ndarray = mention_vector
         self.has_mention_vector: bool = np.equal(mention_vector, np.array([])).all().astype(bool)
@@ -38,18 +38,15 @@ class BaseEntity(IEntity):
             "in_cluster": self.in_cluster,
             "cluster_id": self.cluster_id,
             "has_mention_vector": self.has_mention_vector,
-            "mention_vector": self.mention_vector.tolist() if np.equal(
-                self.mention_vector, np.array([])).all().astype(bool) else []
+            "mention_vector": self.mention_vector.tolist()
         }
 
     @staticmethod
     def from_dict(map: dict) -> IEntity:
-        mention_vector = np.array(
-            map["mention_vector"]) if map["has_mention_vector"] else np.array(
-            [])
+        mention_vector = np.asarray(map["mention_vector"])
         return BaseEntity(
             map["mention"], map["entity_id"], map["entity_source"], map["entity_source_id"],
-            map["cluster_id"], mention_vector)
+            map["cluster_id"], mention_vector, map["has_mention_vector"])
 
     def __str__(self):
         return f"{self.entity_id}-{self.mention}"
